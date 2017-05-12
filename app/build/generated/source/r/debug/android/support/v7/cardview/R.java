fda23901c176cb34f1a9e9a4ef8e16bb25672678
Claudinei B. Junior <claudineibjr@hotmail.com>
2017-05-11 22:32:32 -0300

    Complementação commit anterior

--- app/build/generated/source/r/debug/android/support/v7/cardview/R.java
+++ app/build/generated/source/r/debug/android/support/v7/cardview/R.java
@@ -27,9 +27,9 @@ public final class R {
         public static final int cardview_shadow_start_color = 0x7f0c0016;
     }
     public static final class dimen {
-        public static final int cardview_compat_inset_shadow = 0x7f080071;
-        public static final int cardview_default_elevation = 0x7f080072;
-        public static final int cardview_default_radius = 0x7f080073;
+        public static final int cardview_compat_inset_shadow = 0x7f070073;
+        public static final int cardview_default_elevation = 0x7f070074;
+        public static final int cardview_default_radius = 0x7f070075;
     }
     public static final class style {
         public static final int CardView = 0x7f0900d6;
